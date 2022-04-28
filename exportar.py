402e85d27a4ca8d0938b05698ae71a219103f64b
Alessandro Miranda <alessandro.inovacao@gmail.com>
2022-04-28 17:10:32 -0300

    Ajustes

--- exportar.py
+++ exportar.py
@@ -9,10 +9,11 @@
 
 import pandas as pd         # Biblioteca com funções de ETL
 import pandera as pa        # Biblioteca com funções de ETL
+import csv
+from csv import DictReader
 import sys                  # Biblioteca com funções de sistema
 import psycopg2 as sql      # Permite manipulação de dados Postgres
 import datetime             # para cálculos de tempo usado pelo programa
-
 
 def imprimir_cabecalho():  # Exibe informações iniciais do programa
     print(58*'-')
@@ -38,78 +39,74 @@ def conectar_banco():  # Conecta ao banco de dados e deixa aconexão aberta em "
         sys.exit(0)
     return conexao
 
-# Executa a abertura do arquivo csv e coloca NA e NAN nos valores não informados
-# a fim de facilitar a importação
-def abrir_arquivo_csv(nome_arquivo):
+# Função para inserir dados no banco
+def inserir_db(con,sql):
+    cur = con.cursor()
     try:
-        print('Lendo arquivo ', nome_arquivo)
-        valores_ausentes=['','**','***','****','*****','###!','####','NULL']
-        # Ao encontrar algo especificado em "valores_ausentes", estes serão automaticamente convertidos para Na ou Nan
-        df_planilha = pd.read_csv(nome_arquivo,encoding='ISO-8859-1',sep=';',dayfirst=True,na_values=valores_ausentes,)
-        print('Arquivo lido com sucesso')
+        cur.execute(sql)
+        con.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("** ERROR: %s" % error)
+        con.rollback()
+        cur.close()
+        return 1
+    cur.close()
+
+#exporta dados do dataframe para a tabela informada
+def exportar(conexao,nome_tabela):
+    try:
+        print('Lendo arquivo ', nome_tabela)
+        with open(nome_tabela+'.csv','r') as csv_file:
+            planilha2 = csv.reader(csv_file, delimiter=';',quotechar="'")
+            planilha = DictReader(csv_file)
+            print('Gerando tabela ' + nome_tabela + '...')
+
+            # Extrai os nomes dos campos (linha 1)
+            campos = ''.join(planilha.fieldnames)
+            campos = campos.replace(";",",")
+            qt_colunas = campos.count(',') + 1
+            valores = " values "
+            for row in planilha2:
+                valor = ""
+                valores = valores + "("
+                for coluna in range(qt_colunas):
+                    if (''.join(row[coluna]) == ''):
+                        valor = valor + 'Null, '
+                    else:
+                        valor = valor + "'" + "".join(row[coluna]) + "', "
+                valores = valores + valor
+                valores = valores.replace(", )",")")
+                valores = valores + "),"
+
+            valores = valores.replace(", )", ")")
+            valores = valores[:len(valores)-1]
+            insert = "Insert into " + nome_tabela + " (" + campos + ")" + valores
+
     #Erro do arquivo não existir
     except FileNotFoundError as e:
-        print('*** ERRO: Arquivo ' + nome_arquivo +'.csv não encontrado. Favor verificar.')
+        print('*** ERRO: Arquivo ' + nome_tabela +'.csv não encontrado. Favor verificar.')
         sys.exit()
+
     #Outros erros são exibidos
     except BaseException as e:
         print("*** ERRO: ".format(e))
         sys.exit()
-    return df_planilha
 
-# Verifica se o arquivo tem as colunas nos formatos corretos
-# Se não, mostra erro e encerra o programa
-def validar_arquivo(df_planilha):
-    try:
-        print('Validando arquivo ocorrencia...')
-        schema = pa.DataFrameSchema(
-            columns={"codigo_ocorrencia": pa.Column(pa.Int,nullable=True),
-                     "codigo_ocorrencia2": pa.Column(pa.Int),
-                     "ocorrencia_classificacao": pa.Column(pa.String),
-                     "ocorrencia_cidade": pa.Column(pa.String),
-                     "ocorrencia_uf": pa.Column(pa.String, pa.Check.str_length(2, 2),nullable=True),
-                     "ocorrencia_aerodromo": pa.Column(pa.String, nullable=True),
-                     "ocorrencia_dia": pa.Column(pa.DateTime),
-                     "ocorrencia_hora": pa.Column(pa.String,
-                                                  pa.Check.str_matches(r'^([0-1][0-9]|[2][0-3])(:([0-5][0-9])){1,2}$'),
-                                                  nullable=True),
-                     "total_recomendacoes": pa.Column(pa.Int)
-                     }
-        )
-        schema.validate(df_planilha,lazy=True)
-        print('Arquivo validado com sucesso')
-    except pa.errors.SchemaErrors as e:
-        print('*** Erros encontrados na validação. Favor verificar:')
-        print(58 * '-')
-        print(e.failure_cases)    # erros de dataframe ou schema
-        print(e.data)             # dataframe inválido
-        print(58 * '-')
-        sys.exit()
+    print('Registros incluídos com sucesso')
 
-#exporta dados do dataframe para a tabela informada
-def exportar(conexao,df_planilha,nome_arquivo):
-    print('Gerando tabela ' + nome_arquivo + '...')
-    # Convert dataframe to sql table
-    # Extrai os nomes dos campos (linha 1)
-    Query = "Insert into " +nome_arquivo
-
-    for index, rows in df_planilha.iterrows():
-        # Create list for the current row
-        #my_list = [rows.Date, rows.Event, rows.Cost]
-        print(rows[0])
-
-    df_planilha.to_sql('ocorrencias', conexao, index=False)
-    print('Tabela gerada.')
+    return insert
 
 if __name__ == "__main__":
     tempo_inicial = datetime.datetime.now()
     imprimir_cabecalho()
-    nome_arquivo = "teste.csv"
 
-    df_planilha = abrir_arquivo_csv(nome_arquivo)
-    ## validar_arquivo_ocor(df_planilha) # Caso queira validar
+    ##planilha = abrir_arquivo_csv(nome_tabela)
+    ## validar_arquivo_ocor(planilha) # Caso queira validar
     conexao = conectar_banco()
-    exportar(conexao,df_planilha,"clientes")
+    insert = exportar(conexao,"clientes")
+    retorno = inserir_db(conexao,insert)
+    if (retorno == 1):
+        sys(0)
 
     tempo_final = datetime.datetime.now()
     tempo_total = tempo_final-tempo_inicial
